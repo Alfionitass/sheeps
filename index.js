34fb059107cf1a3ed38672c66a25e1e4eb7beeb9
Alfionitass <alfionitasaadah@gmail.com>
2020-11-10 20:39:44 +0700

    answer sheeps

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function warnTheSheep(queue) {
     // your code here
-    
+    for (let i=0; i< queue.length; i++) {
+        if(queue[queue.length-1] === "wolf") {
+            return "Pls go away and stop eating my sheep"
+        } else if(queue[i] === "wolf") {
+            return `Oi! Sheep number ${queue.length - (i+1)}! You are about to be eaten by a wolf!`
+        }
+    }
 }
 
 
